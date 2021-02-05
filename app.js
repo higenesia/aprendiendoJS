91c45909e5ad03886d4439d22fc75bb2090d64e1
genesis <genesisinais@gmail.com>
2021-02-05 20:53:42 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+var nombre = prompt("Ingresa tu nombre:");
+var apellido = prompt("Ingresa tu apellido:");
+
+console.log(` tu nombre es ${nombre} y tu apellido es ${apellido}`);
+
+/*console.log("Hola "+nombre+ " "+apellido +", bienvenida a Ada!");*/
