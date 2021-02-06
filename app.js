494b5108c2fd2ba2d3cc3168bec51f63f7c9248f
genesis <genesisinais@gmail.com>
2021-02-05 21:57:39 -0300

    ya está listo

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ var apellido = prompt("Ingresa tu apellido:");
 console.log(` tu nombre es ${nombre} y tu apellido es ${apellido}`);
 
 /*console.log("Hola "+nombre+ " "+apellido +", bienvenida a Ada!");*/
+
+var numero1 = 9;
+    numero2 = 3;
+const multiplicación = numero1 * numero2;
+console.log (multiplicacion);
+
+
+let numero1 = prompt("numero 1"),
+    numero2 = prompt ("numero2");
+
+let resultados = number(numero1) + number(numero2);
+
+console.log(`El resultado es ${resultado}`);
